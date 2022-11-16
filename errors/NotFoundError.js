c4454b5a9b0f6bdc10d1559115026c215e0be952
Stas Kudinov <dedbanzai91@mail.ru>
2022-11-16 11:25:25 +0300

    hotfixed: 2.2

--- errors/NotFoundError.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-class NotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'NotFoundError';
-    this.statusCode = 404;
-  }
-}
